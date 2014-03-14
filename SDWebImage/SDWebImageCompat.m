e5f8848320c0cc1cd079ab3e7cbdfb4180960eb6
Michael Henderson <michael@josephmark.com.au>
2014-03-14 13:59:03 +1000

    Insist on scaling images on retina displays.  (SDWebImage only scales if a @2x filename is found. Results in pixelated output)

--- SDWebImage/SDWebImageCompat.m
+++ SDWebImage/SDWebImageCompat.m
@@ -35,8 +35,8 @@ inline UIImage *SDScaledImageForKey(NSString *key, UIImage *image) {
 
             // Modified by michael: show 2x scaled images on retina devices
             // (SDWebImage doesn't automatically scale unless the files are named @2x)
-            //UIImage *scaledImage = [[UIImage alloc] initWithCGImage:image.CGImage scale: [[UIScreen mainScreen] scale] orientation:image.imageOrientation];
-            UIImage *scaledImage = [[UIImage alloc] initWithCGImage:image.CGImage scale: scale orientation:image.imageOrientation];
+            UIImage *scaledImage = [[UIImage alloc] initWithCGImage:image.CGImage scale: [[UIScreen mainScreen] scale] orientation:image.imageOrientation];
+            //UIImage *scaledImage = [[UIImage alloc] initWithCGImage:image.CGImage scale: scale orientation:image.imageOrientation];
             image = scaledImage;
         }
         return image;
